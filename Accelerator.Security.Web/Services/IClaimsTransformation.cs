1427cfe046089842bb52e8720597bf2d4d6dbc2b
Wouter <wouter.vanderauwera@ordina.be>
2020-04-21 10:07:49 +0200

    initial commit

---
+++ Accelerator.Security.Web/Services/IClaimsTransformation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Accelerator.Security.Web.Services
+{
+    public interface IClaimsPrincipal
+    {
+    }
+}
---
+++ Accelerator.Security.Web/Startup.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Accelerator.Security.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            });
+
+            services.AddTransient<IClaimsPrincipal, ClaimsPrincipal>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+
+
+        private async Task<ClaimsPrincipal> TransformClaims(ClaimsPrincipal principal, IServiceCollection services)
+        {
+
+            var transformed = new ClaimsPrincipal();
+            transformed.AddIdentities(principal.Identities);
+            transformed.AddIdentity(new ClaimsIdentity(new Claim[]
+            {
+                new Claim("logged in", DateTime.Now.ToString()) //voorbeeld: claims worden eenmalig gezet bij login
+            }));
+
+            return transformed;
+        }
+    }
+}
